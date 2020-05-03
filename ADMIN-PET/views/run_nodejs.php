85734a3ee640614b77e00e7a4343330164bef925
kevQL <unajmakev@gmail.com>
2020-05-03 11:53:38 -0500

    identando código html

--- ADMIN-PET/views/run_nodejs.php
+++ ADMIN-PET/views/run_nodejs.php
@@ -14,12 +14,12 @@
         include_once("views/modules/navegacion.html");
     ?>
 
-<!-- CUADR DE VIENDENIDA AL USUARIO-->
-<div class="container mt-5">
+    <!-- CUADR DE VIENDENIDA AL USUARIO-->
+    <div class="container mt-5">
 
-    <iframe src="http://localhost:3000/" width="100%" height="700px" frameborder="0" allowfullscreen allow='microphone *; camera *'></iframe>
+        <iframe src="http://localhost:3000/" width="100%" height="700px" frameborder="0" allowfullscreen allow='microphone *; camera *'></iframe>
 
-</div>
+    </div>
 
     <?php        
         include_once('views/modules/cdnsfooter.html');
